210d422cc9264d8f3af7d3e95cbb9e84bb07af4d
felipe.sampaio <felipe.souza@aeb.gov.br>
2016-05-02 11:25:23 -0300

    Efeito icone deezer

---
+++ js/deezer-icon-effect.js
@@ -0,0 +1,11 @@
+function iconHover(){
+  var deezerIcon = document.getElementById('deezer-icon');
+
+  deezerIcon.src = "img/deezer-alaranjado.svg";
+}
+
+function iconHout(){
+  var deezerIcon = document.getElementById('deezer-icon');
+
+  deezerIcon.src = "img/deezer-amarelo.svg";
+}
